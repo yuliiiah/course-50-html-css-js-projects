ffeed14e4bc5755610a3725d8174782cc21c9d63
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-28 23:59:07 +0200

    Section 18: Movie App | Fetching Data From The API

---
+++ movie-app/script.js
@@ -0,0 +1,30 @@
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c20470322b1bf420a3a592fe0ae87370&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=c20470322b1bf420a3a592fe0ae87370&query="`;
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
