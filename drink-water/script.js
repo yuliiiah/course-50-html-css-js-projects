253083f8fa4ccdf5dba642582e9ea0a877c6c0ac
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-28 23:18:07 +0200

    Section 17: Drink Water | Update Big Cup

--- drink-water/script.js
+++ drink-water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remaining = document.getElementById('remaining');
 
+updateBigCup();
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx));
 });
@@ -22,4 +24,30 @@ function highlightCups(idx) {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  console.log(fullCups);
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remaining.style.visibility = 'hidden';
+    remaining.style.height = 0;
+  } else {
+    remaining.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000} L`;
+  }
 }
