44fab0d19de04bcd1c0522905cf003b903bb1128
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-03 21:07:35 +0200

    Section 34: Notes App | Adding Notes To The DOM

---
+++ notes-app/script.js
@@ -0,0 +1,43 @@
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', () => addNewNote());
+
+function addNewNote(text = '') {
+  const note = document.createElement('div');
+  note.classList.add('note');
+
+  note.innerHTML = `
+  <div class="tools">
+    <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+    <button class="delete"><i class="fa-solid fa-trash-can"></i></button>
+  </div>
+
+  <div class="main ${text ? '' : 'hidden'}"></div>
+  <textarea class="${text ? 'hidden' : ''}"></textarea>
+  `;
+
+  const editBtn = note.querySelector('.edit');
+  const deleteBtn = note.querySelector('.delete');
+  const main = note.querySelector('.main');
+  const textArea = note.querySelector('textarea');
+
+  textArea.value = text;
+  main.innerHTML = text;
+
+  deleteBtn.addEventListener('click', () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
+  });
+
+  textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = value;
+  });
+
+  document.body.appendChild(note);
+}
