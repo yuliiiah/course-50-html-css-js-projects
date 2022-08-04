1c49e0e9c512b938f42b14e5c2811424ea62824c
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-04 23:03:33 +0200

    Section 38: Pokedex | Fetching Pokemon Data

---
+++ pokedex/script.js
@@ -0,0 +1,32 @@
+const pokeContainer = document.getElementById('poke-container');
+const pokemonCount = 150;
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data);
+};
